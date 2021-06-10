cac253374dd35cb9acf1bdc7b2ca9f3422002652
Christy Francis <christy23francis@gmail.com>
2021-06-10 13:12:40 +0530

    inital commit1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardLandingComponent } from './dashboard/dashboard-landing/dashboard-landing.component';
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardLandingComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { 
+  
+}
